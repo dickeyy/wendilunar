80f79d775f771ace8445e77209b4b30505798153
Kyle Dickey <kyledickey02@gmail.com>
2024-11-28 08:30:15 +0000

    update server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: vercel(),
+    adapter: node({
+        mode: "standalone"
+    }),
     image: {
         domains: ["shopify.com"]
     },
